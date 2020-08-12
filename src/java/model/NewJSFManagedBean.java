2905979c66fb1621c95d1014b14ca8ee5943b1ef
stiven vargas <stiven1693vargas@gmail.com>
2020-08-12 09:25:35 -0500

    prueba

---
+++ src/java/model/NewJSFManagedBean.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+
+/**
+ *
+ * @author stive
+ */
+@Named(value = "newJSFManagedBean")
+@SessionScoped
+public class NewJSFManagedBean implements Serializable {
+
+    /**
+     * Creates a new instance of NewJSFManagedBean
+     */
+    public NewJSFManagedBean() {
+    }
+    
+    DaoPrueba prueba = new DaoPrueba();
+}
